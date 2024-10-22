cae76931223d72550fa26d24481e05b45813d5be
Prakhar Kumar Srivastava <prakhar25789@gmail.com>
2024-10-22 19:13:28 +0530

    Update DB_URL

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
+
 export const connectDatabase = () => {
-    mongoose.connect('mongodb://localhost:27017/dvd-database')
+    mongoose.connect("mongodb+srv://suyashgupta:<NcyWcljRbw7pp5hy>@cluster0.9sxnw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => {
         console.log("Database connected successfully...");
     })
